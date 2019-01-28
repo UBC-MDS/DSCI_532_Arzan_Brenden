17bcf561f6abb552bf73c23bd936924a0896c10a
everittb <brenden.everitt@gmail.com>
2019-01-28 15:19:44 -0800

    Minor updates

--- City-Crime-Comparison/app.R
+++ City-Crime-Comparison/app.R
@@ -10,7 +10,7 @@
 library(shiny)
 library(tidyverse)
 
-# Load Data
+# Load Data, filter out computed National averages
 ucr_crime <- read.csv("ucr_crime_1975_2015.csv", stringsAsFactors = FALSE)
 ucr_crime <- ucr_crime %>% 
   filter(department_name != "National")
@@ -28,7 +28,7 @@ ucr_avg_crime <- ucr_crime %>%
             avg_robs_100k = mean(rob_per_100k, na.rm = TRUE), avg_agg_ass_100k = mean(agg_ass_per_100k, na.rm = TRUE))
 
 
-# Define UI for application that draws a histogram
+# Define UI for the application
 ui <- fluidPage(
   titlePanel("Crime Rate Analyzer",
              windowTitle = "US Cities Violent Crime"
@@ -36,17 +36,22 @@ ui <- fluidPage(
   ),
   sidebarLayout(
     sidebarPanel(
+      # Add in text stating the data we have
+      helpText("Violent crime data for 68 US police jurisdictions."
+              ),
+      # Create city selection widget
       selectInput("cityInput", label = h3("Select Cities"),
                   choices = cities,
                   multiple = TRUE,
                   selectize = TRUE),
-
+      # Create time frame selection widget
       sliderInput("yearInput", label = h3("Year Range"), step = 5,
                   min = 1975, max = 2015, value = c(1975,2015),
                   sep = ""
       )
     ),
     mainPanel(
+      # Create tabs for the different categories of violent crime 
       tabsetPanel( id = 'tabs', selected = 'violent_crime-violent_per_100k-avg_violent_crime-avg_violent_100k',
         tabPanel("Total", value = 'violent_crime-violent_per_100k-avg_violent_crime-avg_violent_100k', 
                  plotOutput("raw_graph1"), plotOutput("normalized_graph1")),
@@ -63,46 +68,49 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
-  
+  # Create y labels for graphs on different tab panels
   y_labels <- list('violent_crime' = 'Total Violent Crime', 'violent_per_100k' = 'Violent Crime per 100k', 
                    'homs_sum' = 'Total Homicides', 'homs_per_100k' = 'Homicides per 100k',
                    'rape_sum' = 'Total Rapes', 'rape_per_100k' = 'Rapes per 100k',
                    'rob_sum' = 'Total Robberies', 'rob_per_100k' = 'Robberies per 100k',
                    'agg_ass_sum' = 'Total Aggrevated Assualt', 'agg_ass_per_100k' = 'Aggrevated Assualt per 100k')
-  
+  # Get variables to filter data set depending on the selected tab
   variables <- reactive(
     str_split(input$tabs, '-')
   )
-
+  # Filter data for selected cities and year
   ucr_crime_filtered <- reactive(
     ucr_crime %>%
         filter(between(year, input$yearInput[1], input$yearInput[2]),
                department_name %in% input$cityInput)
   )
-  
+  # Filter average data for the selected years
   ucr_avg_crime_filtered <- reactive(
     ucr_avg_crime %>%
       filter(between(year, input$yearInput[1], input$yearInput[2]))
   )
-  
+  # Make raw data plot
   output$raw_graph1 <- output$raw_graph2 <- output$raw_graph3 <- output$raw_graph4 <- output$raw_graph5 <- renderPlot(
       ggplot() +
         geom_line(data = ucr_avg_crime_filtered(), aes_string(x='year', y = variables()[[1]][3]),
                   linetype = "dashed", color = "grey") +
         geom_line(data = ucr_crime_filtered(),
                   aes_string(x='year', y = variables()[[1]][1], group = 'department_name', color = 'department_name'), size = 1.5) +
+        ggtitle("Raw Crime Numbers") +
         xlab('Year') +
         ylab(y_labels[variables()[[1]][1]]) +
         labs(colour = "City") +
         scale_color_brewer(palette = "Spectral", type = "div") +
         theme_bw() + 
         theme(legend.title = element_text(size = 12),
-              legend.text = element_text(size = 11)
+              legend.text = element_text(size = 11),
+              axis.text = element_text(size = 11),
+              plot.title = element_text(size = 13)
               ) 
   )
-  
+  # Make normalized data plot
   output$normalized_graph1 <- output$normalized_graph2 <- output$normalized_graph3 <- output$normalized_graph4 <- output$normalized_graph5 <-  renderPlot(
       ggplot() +
         geom_line(data = ucr_avg_crime_filtered(), aes_string(x='year', y = variables()[[1]][4]),
@@ -110,18 +118,18 @@ server <- function(input, output) {
         geom_line(data = ucr_crime_filtered(), aes_string(x='year', 
                                                               y = variables()[[1]][2], group = 'department_name', 
                                                               color = 'department_name'), size = 1.5) +
+        ggtitle("Normalized Crime Numbers") +
         xlab('Year') +
         ylab(y_labels[variables()[[1]][2]]) +
         labs(colour = "City") +
         scale_color_brewer(palette = "Spectral", type = "div") +
         theme_bw() + 
         theme(legend.title = element_text(size = 12),
-              legend.text = element_text(size = 11)
+              legend.text = element_text(size = 11),
+              axis.text = element_text(size = 11),
+              plot.title = element_text(size = 13)
              )
   )
-  
-  observe(print(variables()[[1]]))
-
 }
 
 # Run the application
